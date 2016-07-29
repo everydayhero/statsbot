19cb0651de2860d7d4d27886b8ac6079386784b3
Keiran <keiran.oleary@everydayhero.com.au>
2016-07-29 11:48:53 +1000

    Add initial bot

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
-const sayHello = () => {
-  console.log('Hello')
-}
+import Botkit from 'botkit'
+import librato from './librato'
 
-sayHello()
+const controller = Botkit.slackbot({
+  debug: false
+})
+
+controller.spawn({
+  token: process.env.SLACK_TOKEN
+}).startRTM()
+
+const interaction = 'direct_message,direct_mention,mention'
+
+controller.hears(['hello', 'hi'], interaction, (bot, message) => {
+  controller.storage.users.get(message.user, (_, user) => {
+    if (user && user.name) {
+      bot.reply(message, `Hello ${user.name}`)
+    } else {
+      bot.reply(message, 'Hello.')
+    }
+  })
+})
+
+controller.hears(['tell me (.*)', '[what’s|whats] (.*)'], interaction, (bot, message) => {
+  const metricName = message.match[1]
+  librato.getMetric(metricName)
+    .then((resp) => {
+      bot.reply(message, '' + resp)
+    })
+    .catch((err) => {
+      bot.reply(message, err.message)
+    })
+})
